6ba0d37a19c7afccd0b9230bb03f41d529d8261e
Muralidhargithub <ot41975@umbc.edu>
2025-01-26 22:39:39 -0500

    first commit

---
+++ WeatherApi-Alamofire/WeatherApi_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApi_AlamofireApp.swift
+//  WeatherApi-Alamofire
+//
+//  Created by Muralidhar reddy Kakanuru on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApi_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
